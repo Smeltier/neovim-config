29281cb0f23cc6a296a46b7993a529affd29118b
Smeltier <connect.goncalves@gmail.com>
2026-02-26 20:36:55 -0300

    add: new plugin for line move.

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-mini/mini.move",
+  event = "VeryLazy",
+  config = function()
+    require("mini.move").setup({
+      mappings = {
+        left = "<M-h>",
+        right = "<M-l>",
+        down = "<M-j>",
+        up = "<M-k>",
+        line_left = "<M-h>",
+        line_right = "<M-l>",
+        line_down = "<M-j>",
+        line_up = "<M-k>",
+      },
+    })
+  end,
+}
